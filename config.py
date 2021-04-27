1761d95f9f50e874eb80ae17bca9960cc8f9a0e7
MugeraH <bwosihughes24@gmail.com>
2021-04-27 14:47:20 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config:
 
 
 class ProdConfig(Config):
-    pass
+     SQLALCHEMY_DATABASE_URI ="postgresql://kwvkjgmotuxlgt:c15f15e7a16f15deeb0897387eedaf7ff0828f50a9255e068c3320f93fae78f0@ec2-54-87-112-29.compute-1.amazonaws.com:5432/d6mdb76iqm30cp?sslmode=require"
 
 
 class TestConfig(Config):
