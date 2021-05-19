787eb896ca82edcda8fa19e2f2ac4f179923f81b
MugeraH <73559321+MugeraH@users.noreply.github.com>
2021-05-19 13:04:51 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 
 export SECRET_KEY="77fdf545-3fd0-4b01-b385-b05ff4e55ab1"
-export MAIL_USERNAME='herotest254@gmail.com'
-export MAIL_PASSWORD="mugera#254"
+export MAIL_USERNAME='heroty7677'
+export MAIL_PASSWORD="ujujujuj8788"
 
 python3 manage.py server 
